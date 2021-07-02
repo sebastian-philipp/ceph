68dc4924486a261311cbb627845ab150605f7617
Patrick Seidensal <pseidensal@suse.com>
2021-07-02 13:18:52 +0200

    cephadm: compile cephadm: compile to $CMAKE_BINARY_DIR/bin/cephadm
    TODO:
    - use python zipapp for compilation
    - fix tests

--- src/cephadm/build.sh
+++ src/cephadm/build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+target_fpath=${1:-cephadm}
+
 cp cephadm.py __main__.py
 zip ca.zip __main__.py
 echo '#!/usr/bin/env python3' | cat - ca.zip >cephadm
 chmod +x cephadm
 rm __main__.py ca.zip
+if [ "${target_fpath}" != "cephadm" ]; then
+    mv cephadm ${target_fpath}
+fi
